ef610863efafc2ba253b054bd0a6dd18290557db
Nari <nariod14gmail.com>
2025-05-24 13:00:40 -0300

    Reworked blueprint listing to split t2 and t3, database changes.

--- ManuOptimizer/models.py
+++ ManuOptimizer/models.py
@@ -2,13 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Blueprint(db.Model):
+    __tablename__ = 'blueprint'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True, nullable=False)
     materials = db.Column(db.JSON, nullable=False)
     sell_price = db.Column(db.Float, nullable=False)
     max = db.Column(db.Integer, nullable=True)
     material_cost = db.Column(db.Float, nullable=False)
+    tier = db.Column(db.String(10), nullable=False, default="T1")  # "T1" or "T2"
+    __mapper_args__ = {
+        "polymorphic_identity": "T1",
+        "polymorphic_on": tier
+    }
 
+class BlueprintT2(Blueprint):
+    __tablename__ = 'blueprint_t2'
+    id = db.Column(db.Integer, db.ForeignKey('blueprint.id'), primary_key=True)
+    invention_chance = db.Column(db.Float, nullable=False)
+    __mapper_args__ = {
+        "polymorphic_identity": "T2",
+    }
 
 class Material(db.Model):
     id = db.Column(db.Integer, primary_key=True)
