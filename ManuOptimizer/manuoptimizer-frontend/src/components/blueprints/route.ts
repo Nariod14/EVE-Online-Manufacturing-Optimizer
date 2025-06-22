992756ee264b43c73d5d176b8127b642d7896cf5
Nari <nariod14gmail.com>
2025-06-22 00:05:01 -0300

    removed some useless files. pruned the repo

--- ManuOptimizer/manuoptimizer-frontend/src/components/blueprints/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { mockBlueprints } from "@/types/blueprints";
-
-export async function GET() {
-  return NextResponse.json(mockBlueprints);
-}
--- ManuOptimizer/manuoptimizer-frontend/src/mocks/initMocks.ts
+++
@@ -1,9 +0,0 @@
-// src/mocks/initMocks.ts
-export async function initMocks() {
-  if (typeof window === 'undefined') return
-
-  const { worker } = await import('./browser')
-  await worker.start({
-    onUnhandledRequest: 'warn' // logs unmocked requests to console!
-  })
-}
